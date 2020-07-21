4b2e3f084bebabf96a1fe7e118786bc4b404d156
Vinay <vinaynu18@gmail.com>
2020-07-20 23:38:21 -0400

    https://app.asana.com/0/1184183482952257/1185182097504391
    This is a test checkin

--- com/source/ctcigayle/StringRotation/StringRotation_II.java
+++ com/source/ctcigayle/StringRotation/StringRotation_II.java
@@ -16,10 +16,13 @@ public class StringRotation_II {
         //if we Concatenate the String two to itself and then find the indexOf(one).. ??
         // time O(1) space O(1) to me
         // CTCI book says time complexity is O(A+B) where A and B are the lengths of the two strings respectively - this is due to the existence of the subString method..
+
         String twoDoubled = two+two;
         if(twoDoubled.indexOf(one)>-1)
             return true;
 
         return false;
     }
+
+    //added a comment for the Asana Integration - https://app.asana.com/0/1184183482952257/1185182097504391
 }
